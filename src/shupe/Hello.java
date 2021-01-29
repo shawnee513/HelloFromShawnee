148b3dad4f2de6ac1c07073c975b3c9793a6255d
Regolith <aircraft247@gmail.com>
2021-01-29 10:56:29 -0700

    Hi from Jacob!

--- src/shupe/Hello.java
+++ src/shupe/Hello.java
@@ -3,5 +3,6 @@ package shupe;
 public class Hello {
     public static void main (String [] args){
         System.out.println("Hello, my name is Shawnee.");
+        System.out.println("Hello, this is Jacob!");
     }
 }
